40a0bcf69c6e86baaa8c8e681b7136dc9f10d0ed
Stephen Kichka <skichka@gmail.com>
2024-02-05 13:44:16 -0500

    moved some configuration back into init.lua

--- init.lua
+++ init.lua
@@ -4,10 +4,25 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure plugins ]]
-require 'lazy-plugins'
+require('lazy').setup({
+  { import = 'custom.plugins' },
+}, {})
 
 -- [[ Setting options ]]
 require 'options'
--- lua/lazy-plugins.lua
+++
@@ -1,9 +0,0 @@
--- [[ Configure plugins ]]
-require('lazy').setup({
-  -- Useful plugin to show you pending keybinds.
-  -- { 'folke/which-key.nvim',  opts = {} },
-
-  { import = 'custom.plugins' },
-}, {})
-
--- vim: ts=2 sts=2 sw=2 et
