1599074fba7163676708a91addfa1d803ff0e1a4
Stephen Kichka <skichka@gmail.com>
2024-02-07 21:34:26 -0500

    added bigfile plugin
    changed the mapping for treesitter context

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
-    vim.keymap.set("n", "[c", function()
+    vim.keymap.set("n", "pc", function()
       require("treesitter-context").go_to_context(vim.v.count1)
     end, { silent = true })
   end
