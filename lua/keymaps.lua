18b304ca98df67cb73e50a74e2ad109b06d7dd9e
Stephen Kichka <skichka@gmail.com>
2024-06-14 16:32:19 -0400

    added keybind to not overwrite yank register during visual selection

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,9 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Don't touch unnamed register when pasting over visual selection
+vim.keymap.set('v', 'p', '"_dP', { silent = true, noremap = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
