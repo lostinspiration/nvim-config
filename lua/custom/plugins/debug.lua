ce4464f0528a76f1a2389c9d267a8a2ad2508f58
Stephen Kichka <skichka@gmail.com>
2024-10-11 22:19:36 -0400

    fix keybind for debugging

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -40,7 +40,7 @@ return {
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-    vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<S-F10>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debug: Step Over' })
     -- vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
