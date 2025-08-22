e09febe8fc0be5fe825045bac5133f3e27bb1b88
Stephen Kichka <skichka@gmail.com>
2025-08-22 15:06:54 -0400

    add in autocomplete functionality

--- init.lua
+++ init.lua
@@ -229,6 +229,69 @@ vim.pack.add({
       end
     },
   },
+  {
+    src = 'https://github.com/L3MON4D3/LuaSnip',
+  },
+  {
+    src = 'https://github.com/saadparwaiz1/cmp_luasnip',
+  },
+  {
+    src = 'https://github.com/hrsh7th/cmp-nvim-lsp',
+  },
+  {
+    src = 'https://github.com/hrsh7th/nvim-cmp',
+    data = {
+      config = function()
+        local cmp = require('cmp')
+        local luasnip = require('luasnip')
+        require('luasnip.loaders.from_vscode').lazy_load()
+        luasnip.config.setup()
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          completion = {
+            completeopt = 'menu,menuone,noinsert',
+          },
+          mapping = cmp.mapping.preset.insert {
+            ['<C-n>'] = cmp.mapping.select_next_item(),
+            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-Space>'] = cmp.mapping.complete {},
+            ['<C-y>'] = cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            },
+            ['<Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { 'i', 's' }),
+          },
+          sources = {
+            { name = 'nvim_lsp' },
+            { name = 'luasnip' },
+            { name = 'path' },
+          },
+        })
+      end
+    },
+  },
 })
 
 -- run config functions for each plugin
@@ -285,7 +348,12 @@ local servers = {
   },
 }
 
+require('mason-lspconfig').setup({
+  ensure_installed = vim.tbl_keys(servers),
+})
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for k, _ in pairs(servers) do
   require('lspconfig')[k].setup({
